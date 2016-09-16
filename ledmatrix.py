91a13f49760b9a074fe4be064d1ddbf7b89e4b75
Dan Visel <dbvisel@gmail.com>
2016-09-16 16:54:54 +0700

    added wiring instructions

--- ledmatrix.py
+++ ledmatrix.py
@@ -4,8 +4,15 @@
 ## you're going to need to follow directions there (on installing SPI)
 ## to get this to work!
 
+## Wire connections:
+## VCC (red wire) goes to +5V
+## GND (black wire) goes to ground
+## DIN (white wire) goes to pin 10
+## CS (gray wire) goes to pin 8
+## CLK (purple wire) goes to pin 11
+
 
 import max7219.led as led
 
 device = led.matrix()
-device.show_message("Hellow world!")
+device.show_message("Hello world!")
