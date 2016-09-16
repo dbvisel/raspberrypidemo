c9ea0bde71dc02be444662e0343991d163106b76
Dan Visel <dbvisel@gmail.com>
2016-09-16 17:10:18 +0700

    added pin instructions

--- ledmatrix.py
+++ ledmatrix.py
@@ -1,8 +1,9 @@
-## this is python 2 code
+## This will only work in Python 2!
 ##
-## from https://github.com/rm-hull/max7219
-## you're going to need to follow directions there (on installing SPI)
-## to get this to work!
+## It's from https://github.com/rm-hull/max7219
+## 
+## If it doesn't work, you're going to need to follow directions there 
+## (on installing SPI) to get this to work!
 
 ## Wire connections:
 ## VCC (red wire) goes to +5V
@@ -10,7 +11,6 @@
 ## DIN (white wire) goes to pin 10
 ## CS (gray wire) goes to pin 8
 ## CLK (purple wire) goes to pin 11
-
 
 import max7219.led as led
 
